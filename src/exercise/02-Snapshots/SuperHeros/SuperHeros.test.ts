c9e291ca96f6080bf2b1791edef2fc6f83fa6298
alexeiddg <117191717+alexeiddg@users.noreply.github.com>
2025-04-05 16:53:39 -0600

    Initial Commit

---
+++ src/exercise/02-Snapshots/SuperHeros/SuperHeros.test.ts
@@ -0,0 +1,19 @@
+/* Exercise 2: Test using snapshots */
+
+/* Mock the function using jest.fn().
+Write three tests inside a describe block. You should use import the superHeros[] and getFlyingSuperHeros function.
+
+1. First Test: 
+Test should return an empty array if no superheros have the 'Fly' power"
+2. Second Test:
+Test should return an array of superHeros that have the 'Fly' power"
+3. Third Test:
+Test should match the snapshot of flying superheros.
+The snapshot file should contain the expected output of the test.
+The snapshot should be saved in a __snapshots__ directory next to the test file.
+The snapshot file should be named SuperHeros.test.ts.snap.
+*/
+
+test("dummy test", () => {
+  expect(true).toBe(true);
+});
